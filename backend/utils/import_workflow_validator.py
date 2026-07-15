33ca80775d3ef84f955c67acac64dfda96ddc79a
Om Purohit <ompurohit787@gmail.com>
2026-07-15 21:00:33 +0530

    BE Date Issue

--- backend/utils/import_workflow_validator.py
+++ backend/utils/import_workflow_validator.py
@@ -1,77 +1,131 @@
+# class ImportWorkflowValidator:
+#
+#     @staticmethod
+#     def validate(data: dict):
+#
+#         print("========== VALIDATOR ==========")
+#         print(data.get("igm_status"))
+#         print(data.get("igm_date"))
+#
+#         # IGM
+#         if data.get("igm_status") == "Done":
+#             if not data.get("igm_date"):
+#                 raise ValueError("IGM Date is required.")
+#
+#
+#         # if data.get("inward_date"):
+#         #
+#         #     if data.get("igm_status") != "Done":
+#         #         raise ValueError(
+#         #             "Complete IGM Status first."
+#         #         )
+#
+#         # BE validation
+#
+#         if data.get("be_no") and not data.get("inward_date"):
+#             raise ValueError("Inward Date is required.")
+#
+#         # Only validate BE Date when both fields are being used.
+#         if data.get("be_no") and not data.get("be_date"):
+#             raise ValueError("BE Date is required.")
+#
+#         if data.get("other_gov_agency") == "Yes":
+#
+#             if not data.get("other_gov_agency_type"):
+#                 raise ValueError(
+#                     "Select Type of Other Gov Agency."
+#                 )
+#
+#         if data.get("assessment_type") == "APR":
+#
+#             if not data.get("cfs_name"):
+#                 raise ValueError(
+#                     "Select CFS Name."
+#                 )
+#
+#         if data.get("co_deface_required") == "Yes":
+#
+#             if data.get("co_deface") != "Done":
+#                 raise ValueError(
+#                     "Complete CO Deface."
+#                 )
+#
+#         if data.get("do_received") == "Done":
+#
+#             if not data.get("do_validity"):
+#                 raise ValueError(
+#                     "DO Validity is required."
+#                 )
+#
+#             if not data.get("do_type"):
+#                 raise ValueError(
+#                     "DO Type is required."
+#                 )
+#
+#         if data.get("transportation") == "Octopus":
+#
+#             if not data.get("transporter"):
+#                 raise ValueError(
+#                     "Select Transporter."
+#                 )
+#
+#         return True
+
+
 class ImportWorkflowValidator:
 
     @staticmethod
     def validate(data: dict):
 
         print("========== VALIDATOR ==========")
-        print(data.get("igm_status"))
-        print(data.get("igm_date"))
+        print(data)
 
-        # IGM
+        # ---------------- IGM ----------------
+
         if data.get("igm_status") == "Done":
             if not data.get("igm_date"):
                 raise ValueError("IGM Date is required.")
 
-
-        # if data.get("inward_date"):
-        #
-        #     if data.get("igm_status") != "Done":
-        #         raise ValueError(
-        #             "Complete IGM Status first."
-        #         )
+        # ---------------- BE ----------------
 
         if data.get("be_no"):
-
             if not data.get("inward_date"):
-                raise ValueError(
-                    "Inward Date is required."
-                )
+                raise ValueError("Inward Date is required.")
 
-        if data.get("be_date"):
+            if not data.get("be_date"):
+                raise ValueError("BE Date is required.")
 
-            if not data.get("be_no"):
-                raise ValueError(
-                    "BE No is required."
-                )
+        # ---------------- Other Govt Agency ----------------
 
         if data.get("other_gov_agency") == "Yes":
-
             if not data.get("other_gov_agency_type"):
-                raise ValueError(
-                    "Select Type of Other Gov Agency."
-                )
+                raise ValueError("Select Type of Other Gov Agency.")
+
+        # ---------------- Assessment ----------------
 
         if data.get("assessment_type") == "APR":
-
             if not data.get("cfs_name"):
-                raise ValueError(
-                    "Select CFS Name."
-                )
+                raise ValueError("Select CFS Name.")
+
+        # ---------------- CO Deface ----------------
 
         if data.get("co_deface_required") == "Yes":
-
             if data.get("co_deface") != "Done":
-                raise ValueError(
-                    "Complete CO Deface."
-                )
+                raise ValueError("Complete CO Deface.")
+
+        # ---------------- DO ----------------
 
         if data.get("do_received") == "Done":
-
             if not data.get("do_validity"):
-                raise ValueError(
-                    "DO Validity is required."
-                )
+                raise ValueError("DO Validity is required.")
 
             if not data.get("do_type"):
-                raise ValueError(
-                    "DO Type is required."
-                )
+                raise ValueError("DO Type is required.")
+
+        # ---------------- Transportation ----------------
 
         if data.get("transportation") == "Octopus":
-
             if not data.get("transporter"):
-                raise ValueError(
-                    "Select Transporter."
-                )
+                raise ValueError("Select Transporter.")
 
         return True
